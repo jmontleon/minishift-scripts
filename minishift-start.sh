0f3cca5b10402aef237945abc3fbed5e5f8fde1a
Jason Montleon <jmontleo@redhat.com>
2017-03-06 13:59:12 -0500

    fix directory creation for 1.5+

--- minishift-start.sh
+++ minishift-start.sh
@@ -4,5 +4,5 @@ source config
 ${MINISHIFT} start
 
 path=/var/lib/origin/openshift.local.pv/pv
-${MINISHIFT} ssh -- "for i in \$(seq -w 1 0100);do mkdir -p ${path}\${i};done"
+${MINISHIFT} ssh -- "for i in \$(seq -w 1 0100);do sudo mkdir -p ${path}\${i};done"
 ${MINISHIFT} ssh -- "sudo chmod 777 ${path}*;done"
