48929f72f325c5496c2bf86476e42ae1fbbbc266
Jason Montleon <jmontleo@redhat.com>
2017-03-06 13:58:07 -0500

    readd directories and fix permissions after every start

--- minishift-start.sh
+++ minishift-start.sh
@@ -2,3 +2,7 @@
 source config
 
 ${MINISHIFT} start
+
+path=/var/lib/origin/openshift.local.pv/pv
+${MINISHIFT} ssh -- "for i in \$(seq -w 1 0100);do mkdir -p ${path}\${i};done"
+${MINISHIFT} ssh -- "sudo chmod 777 ${path}*;done"
